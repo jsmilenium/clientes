b615c1d113734f77ef71c6c48f3c7515a20b9e92
jsmilenium <jsmilenium2003@hotmail.com>
2015-09-14 18:30:50 -0300

    Update Cliente.php

--- Cliente.php
+++ Cliente.php
@@ -1,52 +1,49 @@
 <?php
 
-Class Cliente{
+include 'EnderecoCobranca.php';
 
-	public $nome;
-	public $idade;
-	public $cpf;
-	public $endereco;
-	public $telefone;
+Class Cliente implements EnderecoCobranca{
 
-	public function getNome(){
-		return $this->nome;
-	}
-	
-	public function setNome($nome){
-		$this->nome = $nome;
-	}
-	
-	public function getIdade(){
-		return $this->idade;
-	}
-	
-	public function setIdade($idade){
-		$this->idade = $idade;
-	}
-	
-	public function getCpf(){
-		return $this->cpf;
-	}
-	
-	public function setCpf($cpf){
-		$this->cpf = $cpf;
-	}	
-	
-	public function getEndereco(){
-		return $this->endereco;
-	}
-	
-	public function setEndereco($endereco){
-		$this->endereco = $endereco;
-	}
-	
-	public function getTelefone(){
-		return $this->telefone;
-	}
-	
-	public function setTelefone($telefone){
-		$this->telefone = $telefone;
-	}
+    public $nome;
+    public $idade;
+    public $endereco;
+    public $telefone;
+
+    public function getNome(){
+            return $this->nome;
+    }
+
+    public function setNome($nome){
+            $this->nome = $nome;
+    }
+
+    public function getIdade(){
+            return $this->idade;
+    }
+
+    public function setIdade($idade){
+            $this->idade = $idade;
+    }
+
+    public function getEndereco(){
+            return $this->endereco;
+    }
+
+    public function setEndereco($endereco){
+            $this->endereco = $endereco;
+    }
+
+    public function getTelefone(){
+            return $this->telefone;
+    }
+
+    public function setTelefone($telefone){
+            $this->telefone = $telefone;
+    }
+    
+    public function enderecoCobranca($endereco) {
+        return $endereco;
+    }
 	
 }
 
