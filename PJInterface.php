f6d125092359d4f45bc2b970bdcec06ba46b9fb0
jsmilenium <jsmilenium2003@hotmail.com>
2015-09-15 11:24:37 -0300

    Create PJInterface.php

---
+++ PJInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+interface PJInterface{
+    
+    public function setCNPJ($cnpj);
+    public function getCNPJ();
+    public function setTipo($tipo);
+    public function getTipo();
+}
+
+?>
+
