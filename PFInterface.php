2667d52fe381067a5485a4dadd59030334f16f90
jsmilenium <jsmilenium2003@hotmail.com>
2015-09-15 11:24:07 -0300

    Create PFInterface.php

---
+++ PFInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+interface PFInterface{
+    
+    public function setCPF($cpf);
+    public function getCPF();
+    public function setTipo($tipo);
+    public function getTipo();
+}
+
+?>
+
